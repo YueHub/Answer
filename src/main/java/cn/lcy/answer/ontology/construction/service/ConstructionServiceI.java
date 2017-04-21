37dbd6ae2ac6a64711155c8125c12c2c8c40d27f
YueHub <li_chuan_yue@foxmail.com>
2017-04-21 14:20:49 +0800

    修改部分配置　Linux测试运行
    change some config to run in my linux

--- src/main/java/cn/lcy/answer/ontology/construction/service/ConstructionServiceI.java
+++ src/main/java/cn/lcy/answer/ontology/construction/service/ConstructionServiceI.java
@@ -5,7 +5,7 @@ import cn.lcy.answer.ontology.model.BaikePage;
 public interface ConstructionServiceI {
 
 	// G:/GraduationProject/HanLP/DataAndModel/data/dictionary/custom/Answer_Dict.txt
-	public static final String INDIVIDUAL_UUID_DICT_FILE = "D:/AnswerDict/Answer_Dict.txt";
+	public static final String INDIVIDUAL_UUID_DICT_FILE = "/opt/DeepSearch/ProjectsData/DeepSearch/AnswerDict/Answer_Dict.txt";
 	
 	public boolean construction(BaikePage baikePage) throws Exception;
 	
