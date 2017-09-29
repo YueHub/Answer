fbb84cd6604a595954d5fdbf3a206cb5a3fda22c
Yue <li_chuan_yue@foxmail.com>
2017-09-29 11:32:39 +0800

    delete some unwanted code and design the api data format

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Answer
 
-本科毕业设计－Answer－基于领域本体的语义搜索引擎－Beta0.1
+本科毕业设计－Answer－基于领域本体的语义搜索引擎－Beta 0.9—后台
 
-基于本体的语义搜索引擎-Answer-Betav0.1版-重新迁移至GitHub
+基于本体的语义搜索引擎-Answer-Beta v0.9-重新迁移至GitHub
 
 ![image](https://github.com/YueHub/Answer/blob/master/screenShots/主页.png)
 
--- src/main/java/cn/lcy/answer/AnswerMain.java
+++ src/main/java/cn/lcy/answer/AnswerMain.java
@@ -1,9 +1,7 @@
 package cn.lcy.answer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class AnswerMain {
